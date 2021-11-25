fc292a6a56cefc690418280c779011fc957de329
Santeri Suitiala <santeri.suitiala@voyantic.com>
2021-11-25 18:35:43 +0200

    exercise 1.4

--- src/App.js
+++ src/App.js
@@ -5,49 +5,46 @@ const Header = (props) => {
 };
 
 const Content = (props) => {
-  return props.parts.map((element, index) => (
-    <Part part={element} exercise={props.exercises[index]} />
-  ));
+  return props.parts.map((part) => <Part part={part} />);
 };
 
 const Part = (props) => {
   return (
     <p>
-      {props.part} {props.exercise}
+      {props.part.name} {props.part.exercises}
     </p>
   );
 };
 
 const Total = (props) => {
-  var total = props.exercises.reduce((sum, value) => {
-    return sum + value;
+  var total = props.parts.reduce((sum, part) => {
+    return sum + part.exercises;
   }, 0);
   return <p>Number of exercises {total}</p>;
 };
 
 const App = () => {
   const course = 'Half Stack application development';
-  const part1 = {
-    name: 'Fundamentals of React',
-    exercises: 10
-  };
-  const part2 = {
-    name: 'Using props to pass data',
-    exercises: 7
-  };
-  const part3 = {
-    name: 'State of a component',
-    exercises: 14
-  };
-
-  const names = [part1.name, part2.name, part3.name];
-  const exercises = [part1.exercises, part2.exercises, part3.exercises];
+  const parts = [
+    {
+      name: 'Fundamentals of React',
+      exercises: 10
+    },
+    {
+      name: 'Using props to pass data',
+      exercises: 7
+    },
+    {
+      name: 'State of a component',
+      exercises: 14
+    }
+  ];
 
   return (
     <div>
       <Header course={course} />
-      <Content parts={names} exercises={exercises} />
-      <Total exercises={exercises} />
+      <Content parts={parts} />
+      <Total parts={parts} />
     </div>
   );
 };
