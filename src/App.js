3151957ae5a6b06f5bd31d69fe1d998157acea88
Santeri Suitiala <santeri.suitiala@voyantic.com>
2021-12-21 17:50:09 +0200

    exercise 1.14

--- src/App.js
+++ src/App.js
@@ -2,14 +2,41 @@ import React, { useState } from 'react';
 
 const NextAnecdote = (props) => {
   return (
+    <button
+      onClick={() => {
+        props.setSelected(Math.floor(Math.random() * props.arrayLength));
+      }}
+    >
+      next anecdote
+    </button>
+  );
+};
+
+const VoteCount = ({ votes }) => {
+  return <div>has {votes} votes</div>;
+};
+
+const VoteButton = (props) => {
+  return (
+    <button
+      onClick={() => {
+        const points = [...props.points];
+        points[props.selected]++;
+        props.setPoints(points);
+      }}
+    >
+      vote
+    </button>
+  );
+};
+
+const MostVotedAnecdote = (props) => {
+  const points = props.points;
+  const index = points.indexOf(Math.max(...points));
+  return (
     <div>
-      <button
-        onClick={() => {
-          props.setSelected(Math.floor(Math.random() * props.arrayLength));
-        }}
-      >
-        next anecdote
-      </button>
+      {props.anecdotes[index]}
+      <div>has {points[index]} votes</div>
     </div>
   );
 };
@@ -24,13 +51,17 @@ const App = () => {
     'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
     'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when dianosing patients.'
   ];
-
+  const [points, setPoints] = useState(new Array(anecdotes.length).fill(0));
   const [selected, setSelected] = useState(0);
-
   return (
     <div>
+      <h1>Anecdote of the day</h1>
       {anecdotes[selected]}
+      <VoteCount votes={points[selected]} />
+      <VoteButton setPoints={setPoints} points={points} selected={selected} />
       <NextAnecdote setSelected={setSelected} arrayLength={anecdotes.length} />
+      <h1>Anecdote with most votes</h1>
+      <MostVotedAnecdote points={points} anecdotes={anecdotes} />
     </div>
   );
 };
