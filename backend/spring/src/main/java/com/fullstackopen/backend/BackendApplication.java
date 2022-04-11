74d1f872dd2599b1c0805f9743a6ed63f5f2a409
Santeri Suitiala <santeri.suitiala@almamedia.fi>
2022-04-11 20:15:33 +0300

    Add spring boot project for backend

---
+++ backend/spring/src/main/java/com/fullstackopen/backend/BackendApplication.java
@@ -0,0 +1,14 @@
+package com.fullstackopen.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import com.fullstackopen.backend.Person;
+
+@SpringBootApplication
+public class BackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendApplication.class, args);
+	}
+
+}
