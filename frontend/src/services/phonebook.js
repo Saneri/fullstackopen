afe2742e40d3567cf0272f8a2ab10c0fcb2fd0fe
Santeri Suitiala <santeri.suitiala@almamedia.fi>
2022-05-15 10:49:40 +0300

    add backend to heroku and add dotenv to frontend

--- frontend/src/services/phonebook.js
+++ frontend/src/services/phonebook.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const URL = "http://localhost:3001/api";
+const URL = process.env.REACT_APP_API_URL || "http://localhost:3001/api";
 
 const getAll = () => {
   const request = axios.get(`${URL}/persons`);
